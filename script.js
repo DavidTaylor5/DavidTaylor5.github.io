21da16f50867942bd1493edb42e4b9f41f716b75
David Taylor <davidtaylor@tecace.com>
2025-07-22 13:30:16 -0700

    Remove demo from video titles, update video width, programatically change videos depending on which item is clicked, add videos for TAC and ECO.

--- script.js
+++ script.js
@@ -13,27 +13,47 @@ document.addEventListener("DOMContentLoaded", () => {
   // Open video popup with specific video when project clicked
   document.getElementById('tacomaMusicPlayerProject').onclick = function() {
     document.getElementById('videoPopup').style.display = 'flex';
-    document.getElementById('videoTitle').innerText = 'Tacoma Music Player App Demo';
+    document.getElementById('videoTitle').innerText = 'Tacoma Music Player App';
+    const video = document.getElementById('explanationVideo')
+    video.src =  "videos/tacoma-final-fr.mp4"
+    video.onload();
+    video.onplay();
   };
 
   document.getElementById('ecosystemProject').onclick = function() {
     document.getElementById('videoPopup').style.display = 'flex';
-    document.getElementById('videoTitle').innerText = 'Ecosystem Demo';
+    document.getElementById('videoTitle').innerText = 'Ecosystem';
+    const video = document.getElementById('explanationVideo')
+    video.src =  "videos/ecosystem-final-fr.mp4"
+    video.onload();
+    video.onplay();
   };
 
   document.getElementById('demoGeneratorProject').onclick = function() {
     document.getElementById('videoPopup').style.display = 'flex';
-    document.getElementById('videoTitle').innerText = 'Demo Generator Demo';
+    document.getElementById('videoTitle').innerText = 'Demo Generator';
+    const video = document.getElementById('explanationVideo')
+    video.src =  "videos/tacoma-final-fr.mp4"
+    video.onload();
+    video.onplay();
   };
 
   document.getElementById('gitlabCIProject').onclick = function() {
     document.getElementById('videoPopup').style.display = 'flex';
-    document.getElementById('videoTitle').innerText = 'Gitlab CI/CD Demo';
+    document.getElementById('videoTitle').innerText = 'Gitlab CI/CD';
+    const video = document.getElementById('explanationVideo')
+    video.src =  "videos/tacoma-final-fr.mp4"
+    video.onload();
+    video.onplay();
   };
 
   document.getElementById('espressoProject').onclick = function() {
     document.getElementById('videoPopup').style.display = 'flex';
-    document.getElementById('videoTitle').innerText = 'Espresso UI Demo';
+    document.getElementById('videoTitle').innerText = 'Espresso UI';
+    const video = document.getElementById('explanationVideo')
+    video.src =  "videos/tacoma-final-fr.mp4"
+    video.onload();
+    video.onplay();
   };
 
 });
